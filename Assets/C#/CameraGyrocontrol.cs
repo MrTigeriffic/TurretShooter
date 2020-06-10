019322453a11618c90c1413e49bc76894061cdf0
Oran Chadwick <oranchad@gmail.com>
2020-06-10 17:33:12 +0100

    GunFire, Target, CameraGyro Setup
    Main C# components created with Basic controls.

---
+++ Assets/C#/CameraGyrocontrol.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class CameraGyrocontrol : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Camera>().backgroundColor = new Color()
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/C#/PlayerInput.cs
+++ Assets/C#/PlayerInput.cs
@@ -5,19 +5,34 @@ using UnityEngine.UIElements;
 
 public class PlayerInput : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float damage = 10f;
+    public float range = 100f;
+
+    public Camera fpsCam;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire1") | Input.touchCount > 0)
         {
-            Debug.Log(Input.mousePosition);
+            Touch touch = Input.GetTouch(0);
+            Shoot();
+            //Debug.Log(Input.mousePosition);
         }
        
+    }
+    void Shoot()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+           Target target = hit.transform.GetComponent<Target>();
+           if(target != null)
+            {
+                target.TakeDamage(damage);
+            }
+        }
     }
 }
