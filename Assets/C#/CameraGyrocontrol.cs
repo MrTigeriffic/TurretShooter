57cbb726278fead098e78b8fdf4734ac93c77314
Oran Chadwick <oranchad@gmail.com>
2020-06-15 17:31:20 +0100

    Gyro camera control
    First iteration of gyro camera control without constraints.

--- Assets/C#/CameraGyrocontrol.cs
+++ Assets/C#/CameraGyrocontrol.cs
@@ -1,16 +1,63 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class CameraGyrocontrol : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
+    private bool gyroEnabled;
+    private Gyroscope gyro;
+    private GameObject cameraContainer;
+    private Quaternion rot;
+
     void Start()
     {
-        //GetComponent<Camera>().backgroundColor = new Color();
+        Screen.orientation = ScreenOrientation.LandscapeLeft;
+        cameraContainer = new GameObject("Camera Container");
+        cameraContainer.transform.position = transform.position;
+        transform.SetParent(cameraContainer.transform);
+        //gyro = Input.gyro;
+        gyroEnabled = EnableGyro();
+    }
+
+    private bool EnableGyro()
+    {
+        if (SystemInfo.supportsGyroscope)
+        {
+            gyro = Input.gyro;
+            gyro.enabled = true;
+            cameraContainer.transform.rotation = Quaternion.Euler(90f, 90f, 0f);
+            rot = new Quaternion(0, 0, 1, 0);
+
+            return true;
+        }
+        return false;
+    }
+
+    private void Update()
+    {
+        if (gyroEnabled)
+        {
+            transform.localRotation = gyro.attitude * rot; //update local position of camera 
+        }
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //void Update()
+    //{
+    //    GyroModifyCamera();
+    //    Debug.Log("Rotation Rate: " + m_Gyro.rotationRate);
+    //    Debug.Log("Attitude: " + m_Gyro.attitude);
+    //}
+    //Gyroscope is right handed. Unity is left handed.
+    //Make necessary change to the camera.
+    //private void GyroModifyCamera()
+    //{
+    //    Transform.rotation = GyroToUnity(Input.gyro.attitude);
+    //}
+    //private static Quaternion GyroToUnity(Quaternion q)
+    //{
+    //    return new Quaternion(q.x, q.y, -q.z, -q.w);
+    //}
+
 }
+   
