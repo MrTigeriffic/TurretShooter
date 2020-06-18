b3eb3381518abcf6e586f2ddb35539cabdbf33ad
Oran Chadwick <oranchad@gmail.com>
2020-06-18 16:48:27 +0100

    Ground Object pooling
    Object pooling has been created and needs refinement to work with a ground repeating but does benefit mobile development

---
+++ Assets/C#/GroundRepeat.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundRepeat : MonoBehaviour
+{
+    public float grndRepeat = 1f;
+
+    void Start()
+    {
+
+        InvokeRepeating("Ground", grndRepeat, grndRepeat);
+    }
+
+    void Ground()
+    {
+        //Instantiate(groundObj, transform.position, Quaternion.identity);
+        //find a ground tile that is not active
+        GameObject obj = ObjectPooler.current.GetPooledObject();
+        if (obj == null) return;
+
+        obj.transform.position = transform.position;
+        obj.transform.rotation = transform.rotation;
+        obj.SetActive(true);
+    }
+}
--- Assets/C#/InfiniteGround.cs
+++ Assets/C#/InfiniteGround.cs
@@ -1,4 +1,8 @@
-﻿using System.Collections;
+﻿// Oran Chadwick 18/06/2020
+// Create and Destroy class for ground tiles to create a repeating effect.
+//Upon further research this method of create and destroy is not optimised for mobile development 
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,23 +14,58 @@ public class InfiniteGround : MonoBehaviour
     private Vector3 nextCubeSpawn;
 
     private int randX;
-    private float timer;
-        
+    private float timer = 10f;
+    //private GameObject newInstance;
+    // First attempt of repeating bg worked but the destroy was not working as desired.
+
+    //private BoxCollider2D groundCollider;
+    //private float groundHorizontalLength;
+
     void Start()
     {
         StartCoroutine(spawnTile());
+        //groundCollider = GetComponent<BoxCollider2D>();
+        //groundHorizontalLength = groundCollider.size.x;
     }
 
     void Update()
     {
-        timer += Time.deltaTime;
-        if (timer > 5f)
+        //timer += Time.deltaTime;
+        if (timer >= 0f)
         {
-            Destroy(gameObject);
+
+            timer -= Time.deltaTime;
+            if (timer <= 0f)
+            {
+                Debug.Log("Time");
+                //StartCoroutine(spawnTile());
+                Destruction();
+
+            }
+
         }
         //Not desired outcome as once timer reaches its value gameobject destroys and does not get created again
         // Tags appear to be what I need to look into to destroy 
+        //Debug.Log(gameObject.name + " " + " Child Count: " + gameObject.transform.childCount);
 
+        //if (transform.position.x < -groundHorizontalLength)
+        //{
+        //    RepositionBG();
+        //}
+
+    }
+
+    //private void RepositionBG()
+    //{
+    //    System.Numerics.Vector2 groundOffSet = new System.Numerics.Vector2(groundHorizontalLength * 2f, 0);
+
+    //    //transform.position = (System.Numerics.Vector2)transform.position + groundOffSet;
+    //}
+
+    void Destruction()
+    {
+        Debug.Log("Destroy");
+        Destroy(gameObject);
     }
 
     IEnumerator spawnTile()
@@ -39,7 +78,6 @@ public class InfiniteGround : MonoBehaviour
         yield return new WaitForSeconds(1);//time measured in seconds
         Instantiate(ground1Obj, nextGroundSpawn, ground1Obj.rotation);
         Instantiate(cubeSpawn, nextCubeSpawn, cubeSpawn.rotation); //cube to randomly spawn on the x of the ground tile
-
         nextGroundSpawn.z -= 1000;
         StartCoroutine(spawnTile());
     }
